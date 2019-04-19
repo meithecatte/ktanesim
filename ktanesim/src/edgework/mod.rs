60e972c68de7f9bd04a8839f7257e3c008e7d0f9
Jakub Kądziołka <kuba@kadziolka.net>
2019-04-19 23:31:08 +0200

    Implement edgework battery rendering

--- ktanesim/src/edgework/mod.rs
+++ ktanesim/src/edgework/mod.rs
@@ -1,7 +1,7 @@
 use crate::prelude::*;
 use cairo::ImageSurface;
-use serenity::utils::Colour;
 use lazy_static::lazy_static;
+use serenity::utils::Colour;
 
 pub fn cmd_edgework(
     ctx: &Context,
@@ -18,43 +18,100 @@ pub fn cmd_edgework(
     Ok(())
 }
 
-lazy_static! {
-    static ref SERIAL_NUMBER_TEXTURE: SharedTexture =
-        SharedTexture::new(include_bytes!("Serial number.png"));
-}
-
 const RENDER_SLOT_DIM: (i32, i32) = (269, 136);
 const RENDER_SLOTS: (i32, i32) = (3, 2);
 
 impl crate::bomb::Bomb {
     pub fn render_edgework(&self) -> Render {
-        Render::with(|| {
+        let edgework = self.edgework.clone();
+
+        Render::with(move || {
             let surface = ImageSurface::create(
                 cairo::Format::ARgb32,
                 RENDER_SLOT_DIM.0 * RENDER_SLOTS.0,
                 RENDER_SLOT_DIM.1 * RENDER_SLOTS.1,
-            ).unwrap();
+            )
+            .unwrap();
 
             let ctx = CairoContext::new(&surface);
+            let mut slots = (0..RENDER_SLOTS.0).flat_map(|x| (0..RENDER_SLOTS.1).map(move |y| (x, y)));
+            let mut render = |widget: &dyn Widget| {
+                let (x, y) = slots.next().unwrap();
+                ctx.save();
+                ctx.translate(
+                    (RENDER_SLOT_DIM.0 * x) as f64,
+                    (RENDER_SLOT_DIM.1 * y) as f64,
+                );
 
-            let texture = SERIAL_NUMBER_TEXTURE.to_surface();
+                widget.render(&ctx);
+                ctx.restore();
+            };
 
-            for x in 0..RENDER_SLOTS.0 {
-                for y in 0..RENDER_SLOTS.1 {
-                    ctx.save();
-                    ctx.translate(
-                        (RENDER_SLOT_DIM.0 * x) as f64,
-                        (RENDER_SLOT_DIM.1 * y) as f64,
-                    );
+            render(&edgework.serial_number);
 
-                    ctx.set_source_surface(&texture, 0.0, 0.0);
-                    ctx.paint();
+            for _ in 0..edgework.aa_battery_pairs {
+                let battery = Battery::DoubleAA;
+                render(&battery);
+            }
 
-                    ctx.restore();
-                }
+            for _ in 0..edgework.dcell_batteries {
+                let battery = Battery::DCell;
+                render(&battery);
             }
 
             output_png(surface)
         })
+    }
+}
+
+fn draw_centered_texture(ctx: &CairoContext, texture: &SharedTexture) {
+    ctx.set_source_surface(
+        &texture.to_surface(),
+        ((RENDER_SLOT_DIM.0 - texture.width) / 2) as f64,
+        ((RENDER_SLOT_DIM.1 - texture.height) / 2) as f64,
+    );
+    ctx.paint();
+}
+
+trait Widget {
+    fn render(&self, ctx: &CairoContext);
+}
+
+impl Widget for ktane_utils::edgework::SerialNumber {
+    fn render(&self, ctx: &CairoContext) {
+        lazy_static! {
+            static ref TEXTURE: SharedTexture =
+                SharedTexture::new(include_bytes!("Serial number.png"));
+        }
+
+        draw_centered_texture(ctx, &TEXTURE);
+    }
+}
+
+// The batteries are stored as a count, so we need a "dummy" struct to implement Widget on in order
+// to take advantage of the same pipeline as the other widgets
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
+enum Battery {
+    DCell,
+    // Yes - they come in pairs.
+    DoubleAA,
+}
+
+impl Widget for Battery {
+    fn render(&self, ctx: &CairoContext) {
+        lazy_static! {
+            static ref TEXTURE_AA: SharedTexture =
+                SharedTexture::new(include_bytes!("BatteryAA.png"));
+
+            static ref TEXTURE_D: SharedTexture =
+                SharedTexture::new(include_bytes!("BatteryD.png"));
+        }
+
+        let texture: &SharedTexture = match self {
+            Battery::DoubleAA => &TEXTURE_AA,
+            Battery::DCell => &TEXTURE_D,
+        };
+
+        draw_centered_texture(ctx, texture);
     }
 }
